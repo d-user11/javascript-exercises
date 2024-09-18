9bdb3096f0505f89f69b9a802e29fcbc7d71000f
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-09-18 22:21:20 +0300

    feat: Complete exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (firtstNumber, secondNumber) {
+    if (
+        !Number.isInteger(firtstNumber)
+        || !Number.isInteger(secondNumber)
+        || firtstNumber < 0
+        || secondNumber < 0
+    ) {
+        return 'ERROR';
+    }
 
+    let startNumber;
+    let finishNumber;
+    if (firtstNumber < secondNumber) {
+        startNumber = firtstNumber;
+        finishNumber = secondNumber;
+    } else {
+        startNumber = secondNumber;
+        finishNumber = firtstNumber;
+    }
+
+    let sum = 0;
+    for (let number = startNumber; number <= finishNumber; number++) {
+        sum += number;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
