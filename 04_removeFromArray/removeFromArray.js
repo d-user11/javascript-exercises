9814327b6a5a61680f9dda6ee90ecfbb6a8189fe
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-09-18 21:28:02 +0300

    feat: Complete exercise removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = array.filter((element) => {
+        for (elementToRemove of args) {
+            if (element === elementToRemove) {
+                return false;
+            }
+        }
+        return true;
+    });
+    
+    return newArray;
 };
 
 // Do not edit below this line
