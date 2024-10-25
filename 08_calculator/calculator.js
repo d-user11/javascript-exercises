750ee0ebd3ab6b7073261d8a1062db8f2d34106f
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-10-25 08:35:41 +0300

    feat: Complete exercise calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a = 0, b = 0) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a = 0, b = 0) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, item) => acc * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(nbr) {
+  let fact = 1;
+	if (nbr <= 1) {
+    return fact;
+  }
+  for (i = 1; i <= nbr; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
