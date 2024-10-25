441811970693c4fb4588fab9178549c275f7d081
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-10-25 09:26:24 +0300

    feat: Complete exercise palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const removePunctuation = function (str) {
+    return Array.from(str)
+        .filter((ch) => ch.match(/[a-z0-9]/i))
+        .reduce((newStr, ch) => newStr + ch, '');
+}
 
+const removeWhitespace = function (str) {
+    return str.replace(/\s/g,'');
+}
+
+const palindromes = function (str) {
+    str = removePunctuation(str);
+    str = removeWhitespace(str);
+    str = str.toLowerCase();
+
+    let startIndex = 0;
+    let endIndex = str.length - 1;
+    while (startIndex <= endIndex) {
+        if (str[startIndex] != str[endIndex]) {
+            return false;
+        }
+        startIndex += 1;
+        endIndex -= 1;
+    }
+    return true;
 };
 
 // Do not edit below this line
