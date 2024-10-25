c200d0153f2b63bd88d75b5f9b89075bda7e796f
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-10-25 09:59:18 +0300

    feat: Complete exercise fibonaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (nthNbr) {
+    if (nthNbr < 0) {
+        return 'OOPS';
+    }
 
+    if (nthNbr == 0) {
+        return 0;
+    } else if (nthNbr == 1) {
+        return 1;
+    } else {
+        return fibonacci(nthNbr - 2) + fibonacci(nthNbr - 1);
+    }
 };
 
 // Do not edit below this line
