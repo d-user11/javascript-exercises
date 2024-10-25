832809c07cbe784843ffe8034a78d2d22bf00b9b
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-10-25 23:09:03 +0300

    feat: Complete exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getDeathYear = function (person) {
+    if (person.hasOwnProperty('yearOfDeath')) {
+        return person.yearOfDeath;
+    } else {
+        return new Date().getFullYear();
+    }
+}
 
+const findTheOldest = function (people) {
+    return people.reduce((person, nextPerson) => {
+        let personAge = getDeathYear(person) - person.yearOfBirth;
+        let nextPersonAge = getDeathYear(nextPerson) - nextPerson.yearOfBirth;
+        if (personAge > nextPersonAge) {
+            return person;
+        } else {
+            return nextPerson;
+        }
+    });
 };
 
 // Do not edit below this line
